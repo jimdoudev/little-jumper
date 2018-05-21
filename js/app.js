185e70359aaf22869d0f9c3282925f3e70d6e644
Dimitrios Doumpis <doumpis@hotmail.com>
2018-05-21 22:39:53 +0300

    Write Code Comments

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
+//Declare globals
 let levelcounter = 1;
-// Enemies our player must avoid
+
+// Create Enemy class
 class Enemy {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     constructor(y, speed) {
         this.sprite = 'images/enemy-bug.png';
         this.y = y;
@@ -13,28 +11,21 @@ class Enemy {
     }
 
     // Update the enemy's position, required method for game
-    // Parameter: dt, a time delta between ticks
     update(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    this.x += this.speed * dt;
-    if(this.x > 505){
-        this.x = -50;
         this.x += this.speed * dt;
-    }
+        if (this.x > 505) {
+            this.x = -50;
+            this.x += this.speed * dt;
+        }
     }
 
-    // Draw the enemy on the screen, required method for game
+    // Draw the enemy on the screen
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
+// Create Player class
 class Player {
     constructor() {
         this.sprite = 'images/char-boy.png';
@@ -44,7 +35,7 @@ class Player {
     }
 
     update(dt) {
-        //collision detection
+        //collision detection and changes to remaining lives
         const modal2 = document.getElementById('modal2');
         const lives = document.getElementById('lives');
         const results = document.getElementById("results");
@@ -57,72 +48,75 @@ class Player {
                 this.life -= 1;
                 lives.innerHTML = `<span>Lives: ${this.life}</span>`;
                 results.innerHTML = `<span>Lives Left: ${this.life}</span>`;
-                if(this.life < 1) {
+                if (this.life < 1) {
                     modal2.style.display = 'block';
                 }
             }
-        })
-        //stepping on water
+        });
+
+        //Stepping on water and clearing a level
         const level = document.getElementById('level');
-        if(this.y === -20) {
+        if (this.y === -20) {
             this.x = 200;
             this.y = 405;
             allEnemies.forEach(enemy => {
-                enemy.speed += 20;
+                enemy.speed += 50;
             });
             levelcounter += 1;
-            level.innerHTML = `<span>Level ${levelcounter}/5</span>`
-            };
-
-        //winning the game
-        const modal1 = document.getElementById('modal1');
-        if(levelcounter > 5) {
-            modal1.style.display = "block";
+            level.innerHTML = `<span>Level ${levelcounter}/5</span>`;
         }
-        //changing player
+
+        //Winning the game
+        const modal1 = document.getElementById('modal1');
+        if (levelcounter > 5) {
+            modal1.style.display = "block";
+            level.innerHTML = `<span>Level 5/5</span>`;
+        }
+
+        //Changing player
         const cat = document.querySelector('.cat');
         const boy = document.querySelector('.boy');
         const horn = document.querySelector('.horn');
         const pink = document.querySelector('.pink');
         const princess = document.querySelector('.princess');
 
-        boy.addEventListener('click',() =>{
+        boy.addEventListener('click', () => {
             this.sprite = "images/char-boy.png";
         });
-        cat.addEventListener('click',() =>{
+        cat.addEventListener('click', () => {
             this.sprite = "images/char-cat-girl.png";
         });
-        horn.addEventListener('click',() => {
+        horn.addEventListener('click', () => {
             this.sprite = "images/char-horn-girl.png";
         });
-        pink.addEventListener('click',() => {
+        pink.addEventListener('click', () => {
             this.sprite = "images/char-pink-girl.png";
         });
-        princess.addEventListener('click',() => {
+        princess.addEventListener('click', () => {
             this.sprite = "images/char-princess-girl.png";
         });
     }
 
-    // Draw the enemy on the screen, required method for game
+    //Draw the player on the screen
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 
+    //Implement movement limitations
     handleInput(allowedKeys) {
-        if(allowedKeys === 'left' && this.x > 0) {
+        if (allowedKeys === 'left' && this.x > 0) {
             this.x -= 100;
-        } else if(allowedKeys === 'down' && this.y < 405) {
+        } else if (allowedKeys === 'down' && this.y < 405) {
             this.y += 85;
-        } else if(allowedKeys === 'right' && this.x < 400) {
+        } else if (allowedKeys === 'right' && this.x < 400) {
             this.x += 100;
-        } else if(allowedKeys === 'up' && this.y > 0) {
+        } else if (allowedKeys === 'up' && this.y > 0) {
             this.y -= 85;
         }
     }
 }
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+
+// Instantiate your objects.
 let allEnemies = [
     new Enemy(60, 150),
     new Enemy(145, 100),
